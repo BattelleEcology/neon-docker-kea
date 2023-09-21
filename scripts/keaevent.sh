68ffc23864e2a0361e0393abe7d017f026802389
Steve Jacobs <sjacobs@brokencrew.com>
2023-09-21 10:46:29 -0400

    use new env var for scripts ip

--- scripts/keaevent.sh
+++ scripts/keaevent.sh
@@ -8,7 +8,7 @@ mac=$(echo "${LEASE4_HWADDR}" | tr -d ':')
 event_timestamp=$(date +'%s')
 case "$1" in
   "lease4_select" | "lease4_renew")
-  msg="{\"timestamp\":${event_timestamp},\"valid_lifetime\":${LEASE4_VALID_LIFETIME},\"mac\":\"${mac}\",\"ip\":\"${KEA_LEASE4_ADDRESS}\"}"
+  msg="{\"timestamp\":${event_timestamp},\"valid_lifetime\":${LEASE4_VALID_LIFETIME},\"mac\":\"${mac}\",\"ip\":\"${LEASE4_ADDRESS}\"}"
   # Create lease msg in kafka
   echo -n "${mac}:${msg}" | kcat -b "${KAFKA_BOOTSTRAP_SERVER}" -t "${KAFKA_TOPIC_NAME}" -K: -P -zzstd
   ;;
